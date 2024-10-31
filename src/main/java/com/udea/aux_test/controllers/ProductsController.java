faa77443bbc392901302501127b05fe2da8ed2f1
AndresFSerranoPC <andresfserrano1@gmail.com>
2024-10-30 19:35:30 -0500

    fix: adding cors for localhost:4200

--- src/main/java/com/udea/aux_test/controllers/ProductsController.java
+++ src/main/java/com/udea/aux_test/controllers/ProductsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductsController {
 
     private final IProductService productService;
